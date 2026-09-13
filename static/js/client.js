29d8e27a89c93691095dc54b7ebeaf32e4fe49d5
vibepositive <chrisgervais1@gmail.com>
2026-09-12 22:37:15 -0500

    Bump classified insulin script cache key

--- static/js/client.js
+++ static/js/client.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	// Read-only Trio insulin classification overlay. This changes chart
 	// presentation only; it does not alter Nightscout or Trio dosing behavior.
 	var classifiedInsulin = document.createElement('script');
-	classifiedInsulin.src = 'js/classified-insulin-events.js?v=20260912-clean2';
+	classifiedInsulin.src = 'js/classified-insulin-events.js?v=20260912-native1';
 	classifiedInsulin.async = true;
 	document.body.appendChild(classifiedInsulin);
 });
